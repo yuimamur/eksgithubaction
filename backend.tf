8573c8e2bd7e724be9eda6cbe2292472603d725e
yuimamur <yuuya022221@gmail.com>
2025-10-23 21:05:54 +0900

    9th commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstatetry"
+    bucket = "yuya-test-1023"
     key    = "eks/terraform.tfstate"
     region = "us-west-2"
     //dynamodb_table = "tftable"   # Optional, for state locking
