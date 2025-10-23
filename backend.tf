286166128b29755280ddaf7a8930b773be71b228
yuimamur <yuuya022221@gmail.com>
2025-10-23 21:08:16 +0900

    10th commit

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "yuya-test-1023"
     key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+    region = "us-east-2"
     //dynamodb_table = "tftable"   # Optional, for state locking
     encrypt = true
   }
